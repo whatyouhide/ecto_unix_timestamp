221d842add55084678d6c28896622370227d6d44
Andrea Leopardi <an.leopardi@gmail.com>
2023-12-13 10:50:56 +0100

    Add tests and bulk of features

--- lib/ecto_unix_timestamp.ex
+++ lib/ecto_unix_timestamp.ex
@@ -1,6 +1,35 @@
 defmodule EctoUnixTimestamp do
   @moduledoc """
-  Documentation for `EctoUnixTimestamp`.
+  An Ecto type for datetime fields that are stored and cast as Unix timestamps.
+
+  This type is useful when the data you're casting comes in as a Unix timestamp. In those
+  cases, the built-in Ecto types `:utc_datetime`, `:utc_datetime_usec`, `:naive_datetime`,
+  and `:naive_datetime_usec` work for *storing* the data but not for *casting*. You're forced
+  to pre-parse the parameters that contain the Unix timestamps and convert them into
+  `DateTime` or `NaiveDateTime` structs before passing them to Ecto, which somewhat defeats
+  the purpose of Ecto casting for those fields. This nimble library solves exactly this issue.
+
+  ## Usage
+
+  Use this Ecto type in your schemas. You'll have to choose the **precision** of the Unix
+  timestamp, and the underlying database data type you want to *store* the data as.
+
+      schema "users" do
+        field :created_at, EctoUnixTimestamp, unit: :second, underlying_type: :utc_datetime_usec
+      end
+
+  ## Options
+
+  A `field` whose type is `EctoUnixTimestamp` accepts the following options:
+
+    * `:unit` - The precision of the Unix timestamp. Must be one of `:second`,
+      `:millisecond`, `:microsecond`, or `:nanosecond`. This option is **required**.
+
+    * `:underlying_type` - The underlying Ecto type to use for storing the data.
+      This option is required. It can be one of the native datetime types, that is,
+      `:utc_datetime`, `:utc_datetime_usec`, `:naive_datetime`, or
+      `:naive_datetime_usec`.
+
   """
 
   use Ecto.ParameterizedType
@@ -12,34 +41,45 @@ defmodule EctoUnixTimestamp do
     :nanosecond
   ]
 
-  @impl true
-  def type(_params) do
-    :integer
-  end
+  @valid_underlying_types [
+    :utc_datetime,
+    :utc_datetime_usec,
+    :naive_datetime,
+    :naive_datetime_usec
+  ]
 
   @impl true
   def init(opts) do
-    if not Keyword.has_key?(opts, :unit) do
-      raise ArgumentError, "missing required option :unit"
+    unit =
+      Keyword.get_lazy(opts, :unit, fn ->
+        raise ArgumentError, "missing required option :unit"
+      end)
+
+    underlying_type =
+      Keyword.get_lazy(opts, :underlying_type, fn ->
+        raise ArgumentError, "missing required option :underlying_type"
+      end)
+
+    if unit not in @valid_units do
+      raise ArgumentError, """
+      invalid value for the :unit option, expected one of #{inspect(@valid_units)}, \
+      got: #{inspect(unit)}
+      """
     end
 
-    Enum.each(opts, fn
-      {:unit, unit} ->
-        if unit not in @valid_units do
-          raise ArgumentError, """
-          invalid value for the :unit option, expected one of #{inspect(@valid_units)}, \
-          got: #{inspect(unit)}
-          """
-        end
+    if underlying_type not in @valid_underlying_types do
+      raise ArgumentError, """
+      invalid value for the :underlying_type option, expected one of \
+      #{inspect(@valid_underlying_types)}, got: #{inspect(underlying_type)}
+      """
+    end
 
-      {:field, field} ->
-        {:field, field}
+    %{unit: unit, type: underlying_type}
+  end
 
-      {:schema, schema} ->
-        {:schema, schema}
-    end)
-
-    Map.new(opts)
+  @impl true
+  def type(%{type: type}) do
+    type
   end
 
   @impl true
@@ -49,10 +89,16 @@ defmodule EctoUnixTimestamp do
     {:ok, nil}
   end
 
-  def cast(data, %{unit: unit}) do
+  def cast(data, %{unit: unit, type: type}) do
     case DateTime.from_unix(data, unit) do
-      {:ok, dt} -> {:ok, dt}
-      {:error, _reason} -> :error
+      {:ok, dt} when type in [:naive_datetime, :naive_datetime_usec] ->
+        {:ok, DateTime.to_naive(dt)}
+
+      {:ok, dt} ->
+        {:ok, dt}
+
+      {:error, _reason} ->
+        :error
     end
   end
 
@@ -63,23 +109,14 @@ defmodule EctoUnixTimestamp do
     {:ok, nil}
   end
 
-  def load(data, _loader, %{unit: unit}) do
-    DateTime.from_unix(data, unit)
+  def load(data, _loader, _params)
+      when is_struct(data, DateTime) or is_struct(data, NaiveDateTime) do
+    {:ok, data}
   end
 
   @impl true
-  def dump(data, dumper, params)
-
-  def dump(nil, _dumper, _params) do
-    {:ok, nil}
-  end
-
-  def dump(data, _dumper, %{unit: unit}) do
-    {:ok, DateTime.to_unix(data, unit)}
-  end
-
-  @impl true
-  def equal?(a, b, _params) do
-    a == b
+  def dump(data, _dumper, _params)
+      when is_nil(data) or is_struct(data, DateTime) or is_struct(data, NaiveDateTime) do
+    {:ok, data}
   end
 end
--- test/ecto_unix_timestamp_test.exs
+++ test/ecto_unix_timestamp_test.exs
@@ -13,76 +13,226 @@ defmodule EctoUnixTimestampTest do
     use Ecto.Schema
 
     @primary_key false
-    schema "user_actions" do
+    schema "my_schemas" do
       field :id, :integer, primary_key: true
-      field :timestamp_second, EctoUnixTimestamp, unit: :second
-      field :timestamp_millisecond, EctoUnixTimestamp, unit: :millisecond
-      field :timestamp_microsecond, EctoUnixTimestamp, unit: :microsecond
-      field :timestamp_nanosecond, EctoUnixTimestamp, unit: :nanosecond
+
+      field :timestamp_second_utc, EctoUnixTimestamp,
+        unit: :second,
+        underlying_type: :utc_datetime
+
+      field :timestamp_millisecond_utc, EctoUnixTimestamp,
+        unit: :millisecond,
+        underlying_type: :utc_datetime_usec
+
+      field :timestamp_microsecond_utc, EctoUnixTimestamp,
+        unit: :microsecond,
+        underlying_type: :utc_datetime_usec
+
+      field :timestamp_nanosecond_utc, EctoUnixTimestamp,
+        unit: :nanosecond,
+        underlying_type: :utc_datetime_usec
+
+      field :timestamp_second_naive, EctoUnixTimestamp,
+        unit: :second,
+        underlying_type: :naive_datetime
+
+      field :timestamp_millisecond_naive, EctoUnixTimestamp,
+        unit: :millisecond,
+        underlying_type: :naive_datetime_usec
+
+      field :timestamp_microsecond_naive, EctoUnixTimestamp,
+        unit: :microsecond,
+        underlying_type: :naive_datetime_usec
+
+      field :timestamp_nanosecond_naive, EctoUnixTimestamp,
+        unit: :nanosecond,
+        underlying_type: :naive_datetime_usec
     end
   end
 
   for unit <- [:second, :millisecond, :microsecond] do
-    test "timestamps are casted correctly as integers with unit #{inspect(unit)}" do
+    test "timestamps are casted correctly with unit #{inspect(unit)} (utc)" do
       now = DateTime.utc_now(unquote(unit))
-      field = :"timestamp_#{unquote(unit)}"
+      field = :"timestamp_#{unquote(unit)}_utc"
       params = %{field => DateTime.to_unix(now, unquote(unit))}
 
       changeset = cast(%UserAction{}, params, [field])
       assert changeset.valid?
 
-      user_action = apply_action!(changeset, :validate)
-      assert Map.fetch!(user_action, field) == now
+      my_schema = apply_action!(changeset, :validate)
+      assert Map.fetch!(my_schema, field) == now
+    end
+
+    test "timestamps are casted correctly with unit #{inspect(unit)} (naive)" do
+      now = NaiveDateTime.utc_now(unquote(unit))
+      field = :"timestamp_#{unquote(unit)}_naive"
+
+      params = %{
+        field => now |> DateTime.from_naive!("Etc/UTC") |> DateTime.to_unix(unquote(unit))
+      }
+
+      changeset = cast(%UserAction{}, params, [field])
+      assert changeset.valid?
+
+      my_schema = apply_action!(changeset, :validate)
+      assert Map.fetch!(my_schema, field) == now
+    end
+
+    test "timestamps are validated when casting (with unit #{inspect(unit)})" do
+      field = :"timestamp_#{unquote(unit)}_naive"
+      params = %{field => -377_705_116_801_342_343}
+
+      changeset = cast(%UserAction{}, params, [field])
+      refute changeset.valid?
+      assert {"is invalid", _} = changeset.errors[field]
     end
   end
 
-  test "timestamps are casted correctly as integers with unit :nanosecond" do
+  test "timestamps are casted correctly as integers with unit :nanosecond (utc)" do
     now = DateTime.utc_now()
-    params = %{timestamp_nanosecond: DateTime.to_unix(now, :nanosecond)}
+    params = %{timestamp_nanosecond_utc: DateTime.to_unix(now, :nanosecond)}
 
-    changeset = cast(%UserAction{}, params, [:timestamp_nanosecond])
+    changeset = cast(%UserAction{}, params, [:timestamp_nanosecond_utc])
     assert changeset.valid?
 
-    user_action = apply_action!(changeset, :validate)
-    assert Map.fetch!(user_action, :timestamp_nanosecond) == now
+    my_schema = apply_action!(changeset, :validate)
+    assert Map.fetch!(my_schema, :timestamp_nanosecond_utc) == now
   end
 
-  test "persisting to the database" do
-    start_supervised!({Repo, database: "test/ecto_unix_timestamp_test.sqlite3", log: false})
+  test "timestamps are casted correctly as integers with unit :nanosecond (naive)" do
+    now = NaiveDateTime.utc_now()
+
+    params = %{
+      timestamp_nanosecond_naive: now |> DateTime.from_naive!("Etc/UTC") |> DateTime.to_unix(:nanosecond)
+    }
+
+    changeset = cast(%UserAction{}, params, [:timestamp_nanosecond_naive])
+    assert changeset.valid?
+
+    my_schema = apply_action!(changeset, :validate)
+    assert Map.fetch!(my_schema, :timestamp_nanosecond_naive) == now
+  end
+
+  @tag :tmp_dir
+  test "persisting to the database", %{tmp_dir: tmp_dir} do
+    db_path = Path.join(tmp_dir, "ecto_unix_timestamp_test.sqlite3")
+    start_supervised!({Repo, database: db_path, log: false})
 
     Repo.transaction(fn ->
-      Repo.query!(
-        """
-        CREATE TABLE IF NOT EXISTS user_actions (
-          id INTEGER PRIMARY KEY AUTOINCREMENT,
-          timestamp_second INTEGER,
-          timestamp_millisecond INTEGER,
-          timestamp_microsecond INTEGER,
-          timestamp_nanosecond INTEGER
-        )
-        """,
-        _params = []
+      Repo.query!("""
+      CREATE TABLE IF NOT EXISTS my_schemas (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        timestamp_second_utc INTEGER,
+        timestamp_millisecond_utc INTEGER,
+        timestamp_microsecond_utc INTEGER,
+        timestamp_nanosecond_utc INTEGER,
+        timestamp_second_naive INTEGER,
+        timestamp_millisecond_naive INTEGER,
+        timestamp_microsecond_naive INTEGER,
+        timestamp_nanosecond_naive INTEGER
       )
+      """)
 
-      now = DateTime.utc_now()
+      utc_now = DateTime.utc_now()
+      naive_now = NaiveDateTime.utc_now()
 
-      user_action = %UserAction{
-        timestamp_second: now,
-        timestamp_millisecond: now,
-        timestamp_microsecond: now,
-        timestamp_nanosecond: now
+      my_schema = %UserAction{
+        timestamp_second_utc: DateTime.truncate(utc_now, :second),
+        timestamp_millisecond_utc: DateTime.truncate(utc_now, :millisecond),
+        timestamp_microsecond_utc: utc_now,
+        timestamp_nanosecond_utc: utc_now,
+        timestamp_second_naive: NaiveDateTime.truncate(naive_now, :second),
+        timestamp_millisecond_naive: NaiveDateTime.truncate(naive_now, :millisecond),
+        timestamp_microsecond_naive: naive_now,
+        timestamp_nanosecond_naive: naive_now
       }
 
-      assert {:ok, user_action} = Repo.insert(user_action, returning: true)
+      assert {:ok, my_schema} = Repo.insert(my_schema, returning: true)
 
-      assert Repo.get!(UserAction, user_action.id) == user_action
+      assert Repo.get!(UserAction, my_schema.id) == my_schema
 
-      assert user_action.timestamp_second == DateTime.truncate(now, :second)
-      assert user_action.timestamp_millisecond == DateTime.truncate(now, :millisecond)
-      assert user_action.timestamp_microsecond == DateTime.truncate(now, :microsecond)
-      assert user_action.timestamp_nanosecond == now
+      assert my_schema.timestamp_second_utc == DateTime.truncate(utc_now, :second)
+      assert my_schema.timestamp_millisecond_utc == DateTime.truncate(utc_now, :millisecond)
+      assert my_schema.timestamp_microsecond_utc == DateTime.truncate(utc_now, :microsecond)
+      assert my_schema.timestamp_nanosecond_utc == utc_now
+
+      assert my_schema.timestamp_second_naive == NaiveDateTime.truncate(naive_now, :second)
+      assert my_schema.timestamp_millisecond_naive == NaiveDateTime.truncate(naive_now, :millisecond)
+      assert my_schema.timestamp_microsecond_naive == NaiveDateTime.truncate(naive_now, :microsecond)
+      assert my_schema.timestamp_nanosecond_naive == naive_now
 
       Repo.rollback(:all_good)
     end)
+  end
+
+  test "casting nils" do
+    changeset = cast(%UserAction{}, %{timestamp_second_utc: nil}, [:timestamp_second_utc], empty_values: [])
+    assert changeset.valid?
+  end
+
+  @tag :tmp_dir
+  test "persisting nils to the database", %{tmp_dir: tmp_dir} do
+    db_path = Path.join(tmp_dir, "ecto_unix_timestamp_test.sqlite3")
+    start_supervised!({Repo, database: db_path, log: false})
+
+    Repo.transaction(fn ->
+      Repo.query!("""
+      CREATE TABLE IF NOT EXISTS my_schemas (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        timestamp_second_utc INTEGER,
+        timestamp_millisecond_utc INTEGER,
+        timestamp_microsecond_utc INTEGER,
+        timestamp_nanosecond_utc INTEGER,
+        timestamp_second_naive INTEGER,
+        timestamp_millisecond_naive INTEGER,
+        timestamp_microsecond_naive INTEGER,
+        timestamp_nanosecond_naive INTEGER
+      )
+      """)
+
+      my_schema = %UserAction{}
+
+      assert {:ok, my_schema} = Repo.insert(my_schema, returning: true)
+
+      assert Repo.get!(UserAction, my_schema.id) == my_schema
+
+      assert my_schema.timestamp_second_utc == nil
+      assert my_schema.timestamp_millisecond_utc == nil
+      assert my_schema.timestamp_microsecond_utc == nil
+      assert my_schema.timestamp_nanosecond_utc == nil
+
+      assert my_schema.timestamp_second_naive == nil
+      assert my_schema.timestamp_millisecond_naive == nil
+      assert my_schema.timestamp_microsecond_naive == nil
+      assert my_schema.timestamp_nanosecond_naive == nil
+
+      Repo.rollback(:all_good)
+    end)
+  end
+
+  describe "when validating options" do
+    test "raises if :unit is missing" do
+      assert_raise ArgumentError, "missing required option :unit", fn ->
+        EctoUnixTimestamp.init([])
+      end
+    end
+
+    test "raises if :unit is invalid" do
+      assert_raise ArgumentError, ~r/invalid value for the :unit option/, fn ->
+        EctoUnixTimestamp.init(unit: :invalid_unit, underlying_type: :utc_datetime)
+      end
+    end
+
+    test "raises if :underlying_type is missing" do
+      assert_raise ArgumentError, "missing required option :underlying_type", fn ->
+        EctoUnixTimestamp.init(unit: :second)
+      end
+    end
+
+    test "raises if :underlying_type is invalid" do
+      assert_raise ArgumentError, ~r/invalid value for the :underlying_type option/, fn ->
+        EctoUnixTimestamp.init(unit: :second, underlying_type: :invalid_type)
+      end
+    end
   end
 end
